aa26655bf389bdece4607da1be686dba083cc63b
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-01-17 15:11:14 +0400

    logger installed and first used

--- 4-redux/src/js/client.js
+++ 4-redux/src/js/client.js
@@ -1,38 +1,13 @@
 import { applyMiddleware, createStore } from 'redux';
+import logger from 'redux-logger';
 
-const reducer = (initialState=0, action) => {
-  if(action.type === "INC") {
-    return initialState + 1;
-  } else if(action.type === "DEC") {
-    return initialState - 1;
-  } else if( action.type === "E") {
-    throw new Error("AAAAA!");
-  }
-  return initialState;
+const reducer = (state=0, action) => {
+  
+  return state;
 }
 
-const logger = (store) => (next) => (action) => {
-  console.log("action fired", action);
-  next(action);
-}
+const middleware = applyMiddleware(logger());
 
-const error = (store) => (next) => (action) => {
-  try {
-    next(action);
-  } catch(e) {
-    console.log("AHHHHHH!", e);
-  }
-}
+const store = createStore(reducer, middleware); 
 
-const middleware = applyMiddleware(logger, error);
-
-const store = createStore(reducer, 1, middleware); 
-
-store.subscribe(() => {
-  console.log("store changed", store.getState());
-})
-store.dispatch({type: "INC"});
-store.dispatch({type: "DEC"});
-store.dispatch({type: "DEC"});
-store.dispatch({type: "INC"});
-store.dispatch({type: "E"});
+store.dispatch({type: "FOO"});
