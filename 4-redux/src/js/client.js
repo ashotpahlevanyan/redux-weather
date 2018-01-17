5ba92fc32c5eabe68b8e42b24ceab5942f00ac47
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-01-17 15:16:00 +0400

    actions should have type property, so we add that in our actions

--- 4-redux/src/js/client.js
+++ 4-redux/src/js/client.js
@@ -1,12 +1,12 @@
 import { applyMiddleware, createStore } from 'redux';
 import logger from 'redux-logger';
-
+import thunk from 'redux-thunk';
 const reducer = (state=0, action) => {
   
   return state;
 }
 
-const middleware = applyMiddleware(logger());
+const middleware = applyMiddleware(thunk, logger());
 
 const store = createStore(reducer, middleware); 
 
