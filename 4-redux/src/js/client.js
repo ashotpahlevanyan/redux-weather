0b60f061dbef5ffc55278998192c1394c3a9fc4f
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-01-17 14:19:02 +0400

    added decrement on each store change and called next action

--- 4-redux/src/js/client.js
+++ 4-redux/src/js/client.js
@@ -1,11 +1,27 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 
-const reducer = (initialState=0, action) => {
-
+const reducer = (state=0, action) => {
+  switch(action.type) {
+    case "INC": 
+      return state + 1;
+    case "DEC":
+      return state - 1;
+  }
+  return state;
 }
 
-const store = createStore(reducer, 1); 
+const logger = (store) => (next) => (action) => {
+  console.log("action fired", action);
+  action.type="DEC";
+  next(action);
+}
+const middleware = applyMiddleware(logger);
 
+const store = createStore(reducer, 1, middleware); 
+
+store.subscribe(() => {
+  console.log("store changed", store.getState());
+})
 store.dispatch({type: "INC"});
 store.dispatch({type: "DEC"});
 store.dispatch({type: "DEC"});
