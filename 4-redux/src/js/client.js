7adb59867d1b3c56b25969e2fed01c1fec0b7790
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-01-17 15:09:09 +0400

    middleware is explained

--- 4-redux/src/js/client.js
+++ 4-redux/src/js/client.js
@@ -1,21 +1,30 @@
 import { applyMiddleware, createStore } from 'redux';
 
-const reducer = (state=0, action) => {
-  switch(action.type) {
-    case "INC": 
-      return state + 1;
-    case "DEC":
-      return state - 1;
+const reducer = (initialState=0, action) => {
+  if(action.type === "INC") {
+    return initialState + 1;
+  } else if(action.type === "DEC") {
+    return initialState - 1;
+  } else if( action.type === "E") {
+    throw new Error("AAAAA!");
   }
-  return state;
+  return initialState;
 }
 
 const logger = (store) => (next) => (action) => {
   console.log("action fired", action);
-  action.type="DEC";
   next(action);
 }
-const middleware = applyMiddleware(logger);
+
+const error = (store) => (next) => (action) => {
+  try {
+    next(action);
+  } catch(e) {
+    console.log("AHHHHHH!", e);
+  }
+}
+
+const middleware = applyMiddleware(logger, error);
 
 const store = createStore(reducer, 1, middleware); 
 
@@ -26,3 +35,4 @@ store.dispatch({type: "INC"});
 store.dispatch({type: "DEC"});
 store.dispatch({type: "DEC"});
 store.dispatch({type: "INC"});
+store.dispatch({type: "E"});
