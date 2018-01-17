d45b2d8c335bfbee29b89682bb544c4bf34b3ddb
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-01-17 14:09:18 +0400

    come back to simple app, to explain middleware

--- 4-redux/src/js/client.js
+++ 4-redux/src/js/client.js
@@ -1,36 +1,12 @@
-import { combineReducers, createStore } from 'redux';
+import { createStore } from 'redux';
 
-const userReducer = (state={name: "Ashot", age: 32}, action) => {
-  switch(action.type) {
-    case "CHANGE_NAME": {
-      state = {...state, name : action.payload};
-      break;
-    }
-    case "CHANGE_AGE": {
-      state = {...state, age : action.payload};
-      break;
-    }
+const reducer = (initialState=0, action) => {
 
-  }
-  return state;
 }
 
-const tweetsReducer = (state=[], action) => {
-  return state;
-}
+const store = createStore(reducer, 1); 
 
-const reducers = combineReducers({
-  user: userReducer, 
-  tweets: tweetsReducer
-});
-
-const store = createStore(reducers); 
-
-store.subscribe(() => {
-  console.log("store changed", store.getState());
-})
-
-store.dispatch({type: "CHANGE_NAME", payload: "WILL"});
-store.dispatch({type: "CHANGE_AGE", payload: 367});
-store.dispatch({type: "CHANGE_AGE", payload: 38});
-store.dispatch({type: "CHANGE_NAME", payload: "Ashot"});
+store.dispatch({type: "INC"});
+store.dispatch({type: "DEC"});
+store.dispatch({type: "DEC"});
+store.dispatch({type: "INC"});
